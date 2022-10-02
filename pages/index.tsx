d9a3a34bbe23da2da0a2d5e49be924522adbd310
Elijah Potter <elijah.sirius@protonmail.com>
2022-10-02 14:36:15 -0600

    Improved accessability and SEO for `/`
    Reduced transition time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,44 @@
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
 const iconLinks = [
-  ["https://github.com/chilipepperhott/", "/icons/github.svg"],
-  ["https://www.linkedin.com/in/elijahpotter", "/icons/linkedin.svg"],
-  ["https://www.instagram.com/elijah_sirius/", "/icons/polaroid.svg"],
-  ["/articles", "/icons/type.svg"],
-  ["/videos", "/icons/play.svg"],
+  [
+    "https://github.com/chilipepperhott/",
+    "/icons/github.svg",
+    "GitHub icon linking to my GitHub profile and open source contributions.",
+  ],
+  [
+    "https://www.linkedin.com/in/elijahpotter",
+    "/icons/linkedin.svg",
+    "LinkedIn icon linking to my LinkedIn profile and employment history.",
+  ],
+  [
+    "https://www.instagram.com/elijah_sirius/",
+    "/icons/polaroid.svg",
+    "Poloroid icon linking to my Instagram, where I post the results of my photography hobby.",
+  ],
+  [
+    "/articles",
+    "/icons/type.svg",
+    "Typeface icon linking to my written articles.",
+  ],
+  [
+    "/videos",
+    "/icons/play.svg",
+    "Play icon linking to various demonstrative videos I have made.",
+  ],
 ];
 
 export default function index({ dark }: { dark: boolean }) {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="The personal site for software developer and student Elijah Potter"
+        />
+      </Head>
       <div className="wrapping h-container">
         <div>
           <Image
@@ -19,6 +46,7 @@ export default function index({ dark }: { dark: boolean }) {
             src="/images/headshot.jpg"
             width="300"
             height="300"
+            alt="Headshot of Elijah Potter"
           />
         </div>
         <div className="v-container">
@@ -34,7 +62,7 @@ export default function index({ dark }: { dark: boolean }) {
       </div>
       <div style={{ paddingTop: "100px" }} />
       <div className="wrapping h-container">
-        {iconLinks.map(([href, icon]) => (
+        {iconLinks.map(([href, icon, alt]) => (
           <Link href={href} key={href}>
             <a className="padded">
               <Image
@@ -45,6 +73,7 @@ export default function index({ dark }: { dark: boolean }) {
                 style={{
                   filter: "var(--themefilter)",
                 }}
+                alt={alt}
               />
             </a>
           </Link>
