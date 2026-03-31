7e06698d710fdf9dfbc3167239bf8a638d89a3a4
Elijah Potter <me@elijahpotter.dev>
2026-03-31 12:12:08 -0600

    feat: use database auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GitHub from "next-auth/providers/github";
+import { db } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: DrizzleAdapter(db),
 	providers: [GitHub],
 	callbacks: {
-		jwt({ token, user }) {
-			if (user) {
-				// User is available during sign-in
-				token.id = user.id;
-			}
-			return token;
-		},
-		session({ session, token }) {
-			session.user.id = token.id;
+		session({ session }) {
 			return session;
 		},
 	},
@@ -29,5 +24,5 @@ export async function isAdmin(
 	console.log(session);
 	console.log(process.env.ADMIN_USER_ID);
 
-	return session?.user?.id == process.env.ADMIN_USER_ID;
+	return session?.userId == process.env.ADMIN_USER_ID;
 }
--- src/db/comments.ts
+++ src/db/comments.ts
@@ -1,6 +1,6 @@
 import { db } from ".";
 import { commentsTable, NewComment, Comment } from "./schema";
-import { and, desc, eq, isNotNull, isNotNull, isNull } from "drizzle-orm";
+import { and, desc, eq, isNotNull, isNull } from "drizzle-orm";
 
 export async function insertComment(comment: NewComment): Promise<void> {
 	await db.insert(commentsTable).values(comment);
@@ -10,20 +10,28 @@ export async function getCommentsForPost(postSlug: string): Promise<Comment[]> {
 	return await db
 		.select()
 		.from(commentsTable)
-		.where(and(eq(commentsTable.post, postSlug), isNull(commentsTable.deleted)));
+		.where(
+			and(eq(commentsTable.post, postSlug), isNull(commentsTable.deleted)),
+		);
 }
 
 export async function getCommentFromId(
 	id: number,
 ): Promise<Comment | undefined> {
 	return (
-		await db.select().from(commentsTable).where(and(eq(commentsTable.id, id), isNull(commentsTable.deleted)))
+		await db
+			.select()
+			.from(commentsTable)
+			.where(and(eq(commentsTable.id, id), isNull(commentsTable.deleted)))
 	)[0];
 }
 
 /** Performs a soft delete by setting the delete timestamp to now. */
 export async function deleteComment(id: number): Promise<void> {
-  await db.update(commentsTable).set({ deleted: new Date()}).where(and(eq(commentsTable.id, id), isNull(commentsTable.deleted)));
+	await db
+		.update(commentsTable)
+		.set({ deleted: new Date() })
+		.where(and(eq(commentsTable.id, id), isNull(commentsTable.deleted)));
 }
 
 export async function getRecentComments(limit: number): Promise<Comment[]> {
@@ -31,6 +39,6 @@ export async function getRecentComments(limit: number): Promise<Comment[]> {
 		.select()
 		.from(commentsTable)
 		.orderBy(desc(commentsTable.timestamp))
-    .where(isNull(commentsTable.deleted))
+		.where(isNull(commentsTable.deleted))
 		.limit(limit);
 }
